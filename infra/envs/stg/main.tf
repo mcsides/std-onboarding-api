7ca250d042401092e9ca5b78a614293ed99829eb
Captain Mcwiise <mcwiise@gmail.com>
2025-06-11 19:35:05 -0500

    feat: add deployment stage

--- infra/envs/stg/main.tf
+++ infra/envs/stg/main.tf
@@ -39,3 +39,26 @@ module "ecs_service_nginx" {
   svc_subnets             = [data.terraform_remote_state.stamper_labs_prod.outputs.subnet_id]
   svc_security_groups     = [data.terraform_remote_state.stamper_labs_stg.outputs.allow_http_security_group_id]
 }
+
+module "ecs_task_definition_onboarding_api" {
+  source                   = "../../module/ecs_task_definition"
+  td_family                = "std-stg-ecs-ftask-onboarding-api"
+  td_network_mode          = "awsvpc"
+  td_compatibilities       = ["FARGATE"]
+  td_cpu                   = "256"
+  td_memory                = "512"
+  td_container_definitions = file("./container/nginx.json")
+  td_execution_role_arn = data.terraform_remote_state.stamper_labs_stg.outputs.ecs_task_execution_role_arn
+  env_tag                  = "stg"
+}
+
+module "ecs_service_onboarding_api" {
+  source                  = "../../module/ecs_service"
+  svc_name                = "std-stg-ecs-fsvc-onboarding-api"
+  svc_cluster_id          = data.terraform_remote_state.stamper_labs_stg.outputs.ecs_cluster_id
+  svc_task_definition_arn = module.ecs_task_definition_onboarding_api.task_definition_arn
+  svc_launch_type         = "FARGATE"
+  svc_desired_count       = 3
+  svc_subnets             = [data.terraform_remote_state.stamper_labs_prod.outputs.subnet_id]
+  svc_security_groups     = [data.terraform_remote_state.stamper_labs_stg.outputs.allow_http_security_group_id]
+}
