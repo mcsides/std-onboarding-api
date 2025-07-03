4a843f40ed6b01ac22d612522446dd5a3edc9b62
Captain Mcwiise <mcwiise@gmail.com>
2025-07-02 21:41:05 -0500

    fix: outputs

--- infra/envs/staging/outputs.tf
+++ infra/envs/staging/outputs.tf
@@ -2,14 +2,14 @@ output "std_onboarding_api_ecs_task_definition_arn" {
   value = module.std_onboarding_api_ecs_task_definition.arn
 }
 
-output "stamper_vpc_subnet_b_private_id" {
-  value = data.terraform_remote_state.base_infra.outputs.stamper_vpc_subnet_b_private_id
+output "stamper_vpc_subnet_a_public_id" {
+  value = data.terraform_remote_state.base_infra.outputs.stamper_vpc_subnet_a_public_id
 }
 
 output "stamper_vpc_security_group_id" {
   value = data.terraform_remote_state.base_infra.outputs.stamper_vpc_security_group_id
 }
 
-output "std_ecs_cluster_staging_name" {
-  value = data.terraform_remote_state.base_infra.outputs.std_ecs_cluster_staging_name
+output "cnd_ecs_cluster_staging_name" {
+  value = data.terraform_remote_state.base_infra.outputs.cnd_ecs_cluster_staging_name
 }
